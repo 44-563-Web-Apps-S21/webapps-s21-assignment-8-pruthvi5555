fbfc64dfe2dd2d4112fdb243aa354751a8f0da6e
pruthvi5555 <77819499+pruthvi5555@users.noreply.github.com>
2021-03-15 19:54:07 -0500

    touristplace

--- app2.js
+++ app2.js
@@ -9,11 +9,16 @@ const server =
       res.statusCode = 200      //code for OK
       res.setHeader('Content-Type', 'text/html') //Set the mime type HTML
 
-      res.write('<html> <head> <title> Served </title> </head>')
+      res.write('<html> <head><style>table { margin-top:30;margin-left:50px; border: solid 4px rgb(105,206,145)}</style> <title> Served </title> </head>')
       res.write('<body>')
-      res.write('Content \n')
-      res.write('More content \n')
-      res.write('Hello World')    
+      res.write('<table>')
+      res.write('<tr><th>Location </th> <th> Number of hours that <br> visitor should spend </th> <th> Amount of money someone <br> is expected to pay</th> </tr>  \n')
+      res.write('<tr><td>Pune   </td> <td> 14hours </td><td> $350 </td> </tr>')
+      res.write('<tr><td>New Delhi   </td> <td> 16hours </td> <td> $300 </td>')
+      res.write('<tr><td>Banglore   </td> <td> 14hours </td> <td> $290 </td>')
+      res.write('<tr><td>Mumbai   </td> <td> 12hours </td> <td> $300 </td>')  
+      res.write('<tr><td>Hyderabad   </td> <td> 15hours </td> <td> $ 310 </td>')
+      res.write('</table>')  
       res.end('</body></html>')
       //Close the response
     }                           
